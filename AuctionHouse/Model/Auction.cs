3c2f1adf5560848c1967aafd46bdfc0ab2f0d962
troxdk <desmaagraa@gmail.com>
2014-09-12 11:47:23 +0200

    1. draft to model
    Added function to modelitems.

--- AuctionHouse/Model/Auction.cs
+++ AuctionHouse/Model/Auction.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class Auction
+    public class Auction
     {
+        public delegate void BiddingDel();
+
+        public event BiddingDel NewRound;
+        public event BiddingDel NewBidAccepted;
+
+        private Queue<AuctionItem> auctionItems;
+
+        private AuctionItem _currentItem;
+
+        public AuctionItem CurrentItem
+        {
+            get { return _currentItem; }
+            set { _currentItem = value; }
+        }
+        
+
+        public Auction(Queue<AuctionItem> Items)
+        {
+            auctionItems = Items;
+        }
+
+        public void Start()
+        {
+            _currentItem = auctionItems.Dequeue();
+
+            NewRound();
+        }
+
+        public bool PlaceBid(decimal bid)
+        {
+            bool success = CurrentItem.PlaceBid(bid);
+
+            if (success == true)
+            {
+                NewBidAccepted();
+            }
+
+            return success;
+        }
+
+
+
+
     }
 }
--- AuctionHouse/Model/AuctionItem.cs
+++ AuctionHouse/Model/AuctionItem.cs
@@ -6,7 +6,68 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class AuctionItem
+    public class AuctionItem
     {
+        private string _itemName;
+        private decimal _bid;
+        private bool _sold;
+        private decimal _minimumSoldPrice;
+
+	    public decimal MinimunSoldPrice
+	    {
+		    get { return _minimumSoldPrice;}
+		    set { _minimumSoldPrice = value;}
+	    }
+
+        public bool Sold
+        {
+            get { return _sold; }
+            set { _sold = value; }
+        }
+        
+        public string ItemName
+        {
+            get { return _itemName; }
+            set { _itemName = value; }
+        }
+
+        public decimal Bid
+        {
+            get { return _bid; }
+            set { _bid = value; }
+        }
+        
+        public AuctionItem(string itemName, int bid, decimal minimumSoldPrice)
+        {
+            ItemName = itemName;
+            Bid = bid;
+            MinimunSoldPrice = minimumSoldPrice;
+        }
+
+        public bool PlaceBid(decimal newBid)
+        {
+            if (newBid > Bid && Sold == false)
+            {
+                Bid = newBid;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsSold()
+        {
+            if (Bid > MinimunSoldPrice)
+            {
+                Sold = true;
+            }
+            else
+            {
+                Sold = false;
+            }
+            return Sold;
+        }
     }
 }
